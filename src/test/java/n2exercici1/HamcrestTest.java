3a488632891583e563a574a7191fc7b18f1ade31
ATomas87 <kombayn_nikoladze@msn.com>
2022-05-11 15:44:59 +0200

    Nivel 2 terminado

---
+++ src/test/java/n2exercici1/HamcrestTest.java
@@ -0,0 +1,25 @@
+package n2exercici1;
+
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class HamcrestTest {
+
+    @Test
+    public void testHamcrest() {
+        assertThat("Mordor", length(is(8)));
+    }
+    public static Matcher<String> length(Matcher<? super Integer> matcher) {
+        return new FeatureMatcher<String, Integer>(matcher, "a String of length that", "length") {
+            @Override
+            protected Integer featureValueOf(String actual) {
+                return actual.length();
+            }
+        };
+    }
+}
